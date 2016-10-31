fdef88bce03d5c77a337b50a092ee5cdf3aef90d
tom-workman <tom.workman@us.ibm.com>
2016-10-31 14:27:38 -0500

    Edit search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,6 +11,7 @@ class SearchBar extends Component {
     return (
       <div className="search-bar">
         <input
+          placeholder="Search for your favorite YouTube videos!"
           value={this.state.term}
           onChange={event => this.onInputChange(event.target.value)} />
       </div>
